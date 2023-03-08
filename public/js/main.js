b05e715ef4603ca25117a20677bdc7da0aa22703
danny <danny@dannys-MacBook-Pro.local>
2023-03-07 18:03:15 -0800

    left: index and contact

--- public/js/main.js
+++ public/js/main.js
@@ -22,33 +22,23 @@
 
 function myFunction() {
   var x = document.getElementById("myLinks");
+  var y = document.getElementById("content");
+  var z = document.getElementById("content_home");
   if (x.style.display === "block") {
     x.style.display = "none";
+    if (y) {
+      y.style.display = "block";
+    }
+    if (z) {
+      z.style.display = "block";
+    }
   } else {
     x.style.display = "block";
+    if (y) {
+      y.style.display = "none";
+    }
+    if (z) {
+      z.style.display = "none";
+    }
   }
-}
-
-let slideIndex = [1, 1];
-let slideId = ["mySlides1", "mySlides2"];
-showSlides(1, 0);
-showSlides(1, 1);
-
-function plusSlides(n, no) {
-  showSlides((slideIndex[no] += n), no);
-}
-
-function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {
-    slideIndex[no] = 1;
-  }
-  if (n < 1) {
-    slideIndex[no] = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex[no] - 1].style.display = "block";
 }
--- public/js/slider.js
+++ public/js/slider.js
@@ -1,32 +1,43 @@
-const slideGallery = document.querySelector(".slides");
-const slides = slideGallery.querySelectorAll("div");
-const thumbnailContainer = document.querySelector(".thumbnails");
-const slideCount = slides.length;
-const slideWidth = 540;
+const galleries =
+  document.querySelectorAll("#project_detail").length > 0
+    ? document.querySelectorAll("#project_detail")
+    : document.querySelectorAll("#product_detail");
 
-const highlightThumbnail = () => {
-  thumbnailContainer
-    .querySelectorAll("div.highlighted")
-    .forEach((el) => el.classList.remove("highlighted"));
-  const index = Math.floor(slideGallery.scrollLeft / slideWidth);
-  thumbnailContainer
-    .querySelector(`div[data-id="${index}"]`)
-    .classList.add("highlighted");
+const highlightThumbnailMulti = () => {
+  galleries.forEach((e) => {
+    const slideGallery = e.querySelector(".slides");
+    const slides = slideGallery.querySelectorAll("div");
+    const thumbnailContainer = e.querySelector(".thumbnails");
+    const slideCount = slides.length;
+    const slideWidth = screen.width > 1290 ? screen.width / 3 : screen.width;
+
+    const highlightThumbnail = () => {
+      thumbnailContainer
+        .querySelectorAll("div.highlighted")
+        .forEach((el) => el.classList.remove("highlighted"));
+      const index = Math.floor(slideGallery.scrollLeft / slideWidth);
+      thumbnailContainer
+        .querySelector(`div[data-id="${index}"]`)
+        .classList.add("highlighted");
+    };
+
+    const scrollToElement = (el) => {
+      const index = parseInt(el.dataset.id, 10);
+      slideGallery.scrollTo(index * slideWidth, 0);
+    };
+
+    thumbnailContainer.innerHTML += [...slides]
+      .map((slide, i) => `<div data-id="${i}"></div>`)
+      .join("");
+
+    thumbnailContainer.querySelectorAll("div").forEach((el) => {
+      el.addEventListener("click", () => scrollToElement(el));
+    });
+
+    slideGallery.addEventListener("scroll", (e) => highlightThumbnail());
+
+    highlightThumbnail();
+  });
 };
 
-const scrollToElement = (el) => {
-  const index = parseInt(el.dataset.id, 10);
-  slideGallery.scrollTo(index * slideWidth, 0);
-};
-
-thumbnailContainer.innerHTML += [...slides]
-  .map((slide, i) => `<div data-id="${i}"></div>`)
-  .join("");
-
-thumbnailContainer.querySelectorAll("div").forEach((el) => {
-  el.addEventListener("click", () => scrollToElement(el));
-});
-
-slideGallery.addEventListener("scroll", (e) => highlightThumbnail());
-
-highlightThumbnail();
+highlightThumbnailMulti();

f93983a18b59024b093e039f66e7860bdfad96da
danny <danny@dannys-MacBook-Pro.local>
2023-03-08 23:30:13 -0800

    contact all that's left

--- public/js/slider.js
+++ public/js/slider.js
@@ -9,34 +9,36 @@ const highlightThumbnailMulti = () => {
     const slides = slideGallery.querySelectorAll("div");
     const thumbnailContainer = e.querySelector(".thumbnails");
     const slideCount = slides.length;
-    const slideWidth = screen.width > 1290 ? screen.width / 3 : screen.width;
+    const slideWidth = screen.width > 1290 ? screen.width / 3.5 : screen.width;
 
-    const highlightThumbnail = () => {
-      thumbnailContainer
-        .querySelectorAll("div.highlighted")
-        .forEach((el) => el.classList.remove("highlighted"));
-      const index = Math.floor(slideGallery.scrollLeft / slideWidth);
-      thumbnailContainer
-        .querySelector(`div[data-id="${index}"]`)
-        .classList.add("highlighted");
-    };
+    if (slides.length > 1) {
+      const highlightThumbnail = () => {
+        thumbnailContainer
+          .querySelectorAll("div.highlighted")
+          .forEach((el) => el.classList.remove("highlighted"));
+        const index = Math.floor(slideGallery.scrollLeft / slideWidth);
+        thumbnailContainer
+          .querySelector(`div[data-id="${index}"]`)
+          .classList.add("highlighted");
+      };
 
-    const scrollToElement = (el) => {
-      const index = parseInt(el.dataset.id, 10);
-      slideGallery.scrollTo(index * slideWidth, 0);
-    };
+      const scrollToElement = (el) => {
+        const index = parseInt(el.dataset.id, 10);
+        slideGallery.scrollTo(index * slideWidth, 0);
+      };
 
-    thumbnailContainer.innerHTML += [...slides]
-      .map((slide, i) => `<div data-id="${i}"></div>`)
-      .join("");
+      thumbnailContainer.innerHTML += [...slides]
+        .map((slide, i) => `<div data-id="${i}"></div>`)
+        .join("");
 
-    thumbnailContainer.querySelectorAll("div").forEach((el) => {
-      el.addEventListener("click", () => scrollToElement(el));
-    });
+      thumbnailContainer.querySelectorAll("div").forEach((el) => {
+        el.addEventListener("click", () => scrollToElement(el));
+      });
 
-    slideGallery.addEventListener("scroll", (e) => highlightThumbnail());
+      slideGallery.addEventListener("scroll", (e) => highlightThumbnail());
 
-    highlightThumbnail();
+      highlightThumbnail();
+    }
   });
 };
 
